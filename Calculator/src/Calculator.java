69337f14f79603b441b84dcd87fc415f0973376b
Kulbhushan27 <92461660+Kulbhushan27@users.noreply.github.com>
2021-11-20 16:16:10 +0530

    Add files via upload

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,15 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum1 = add(1, 2);
+	}
+
+	private static int add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+
+}
